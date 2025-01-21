9edbd8205c5c512dfdf5439fe49b5257a2cc2a01
davimdolabella <davimdolabella03@gmail.com>
2025-01-21 16:08:59 -0300

    models

--- quizmania/admin.py
+++ quizmania/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
-
-# Register your models here.
+from . import models
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+@admin.register(models.Difficulty)
+class DifficultyAdmin(admin.ModelAdmin):
+    ...
+    
+@admin.register(models.Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    ...
+    
+@admin.register(models.Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = [ 'question', 'difficulty', 'quiz']
+    
+@admin.register(models.Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ['answer','question']
+     
+@admin.register(models.Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    ...
+     
--- quizmania/models.py
+++ quizmania/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+    def __str__(self):
+        return self.name
+
+class Difficulty(models.Model):
+    name = models.CharField(max_length=65)
+    def __str__(self):
+        return self.name
+
+class Quiz(models.Model):
+    def __str__(self):
+        return f"{self.title}({self.category})"
+    title = models.CharField(max_length=20)
+    description = models.TextField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    cover = models.ImageField(upload_to='quiz/covers/%Y/%m/%d/', blank=True, default='')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, related_name='quizes', null=True
+    )
+
+class Question(models.Model):
+    def __str__(self):
+        return self.question
+    question = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    difficulty = models.ForeignKey(
+        Difficulty, on_delete=models.CASCADE, related_name='questions' 
+    )
+    quiz = models.ForeignKey(
+        Quiz, on_delete=models.CASCADE, related_name='questions'
+    )
+
+class Answer(models.Model):
+    def __str__(self):
+        return self.answer
+    answer = models.CharField(max_length=100)
+    is_correct = models.BooleanField(default=False)
+
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name='answers'
+    )    
+
+class Profile(models.Model):
+    def __str__(self):
+        return f'profile of {self.user}'
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profile'
+    )
+    competed_quizes = models.ManyToManyField(
+        Quiz, blank=True, related_name='profiles'
+    )
+    correct_questions = models.ManyToManyField(
+        Question, blank=True, related_name='profiles'
+    )
+    points = models.IntegerField(default=0)
+    ranking = models.PositiveIntegerField(blank=True, null=True)
